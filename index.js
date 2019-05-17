00287ac7804dbf630c6bf2b88195992696f98d73
longhaiyan <1250766229@qq.com>
2019-05-18 00:33:44 +0800

    add: 添加全局指令

--- index.js
+++ index.js
@@ -1,64 +1,18 @@
-const readline = require('readline');
-const fs = require('fs');
-let text = ''
+#!/usr/bin/env node
 
-const typeMap = {
-  /**
-   * 有序列表
-   */
-  orderList: {
-    reg: /^(\d\s*\.\s*)/, // 正则匹配表达式
-  },
-  /**
-   * title
-   */
-  title: {
-    reg: /^(\s*\#+)/
-  },
-  /**
-   * 被标识是英文单词或片段
-   */
-  word: {
-    reg: /\b(\w|\+|\-|\*|\s|\%|\/)+\b/g
-  }
-}
+const fs = require('fs');
+var path = require('path');
+const readline = require('readline');
+const typeMap = require('./lib/type-map')
+const { regWord, checkLine } = require('./lib/tool')
+const inputPath = process.argv[2] || 'test.md'
+const outputPath = process.argv[3] || 'test1.md'
+let text = '';
+
 const rl = readline.createInterface({
-  input: fs.createReadStream('test.md'),
+  input: fs.createReadStream(path.resolve(process.cwd(), inputPath)),
   crlfDelay: Infinity
 });
-
-function regWord(line) {
-  let reg = typeMap.word.reg;
-  let regResult = null
-
-  let results = []
-  let start = checkLine(line, typeMap.orderList.reg)
-  let result = start ? line.replace(start, '') : line
-
-  while ((regResult = reg.exec(line)) != null) {
-    results.push(regResult[0])
-  }
-  if (results.length) {
-    results.map(key => {
-      result = result.replace(key, "`" + key + "`")
-    })
-
-  }
-
-  result = start ? start + result : result
-
-  return result
-}
-
-/**
- * 检查当前行的类型
- * @param {string} line 要检查的内容
- * @param {reg} reg 正则表达式
- */
-function checkLine(line, reg) {
-  let result = reg.exec(line)
-  return result ? result[0] : false
-}
 
 
 rl.on('line', (line) => {
@@ -70,10 +24,10 @@ rl.on('line', (line) => {
 
 });
 rl.on('close', () => {
-  fs.writeFile('test1.md', text, (err, stats) => {
+  fs.writeFile(path.resolve(process.cwd(), outputPath), text, (err, stats) => {
     if (err) {
       return console.error(err);
     }
   });
-  console.log('test1.md 文件已生成')
+  console.log(`${outputPath} 文件已生成`)
 })
