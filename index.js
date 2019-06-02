315956376b177cce796f846db1fefb37c6422930
longhaiyan <1250766229@qq.com>
2019-06-02 23:56:03 +0800

    v0.0.3

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const TurndownService = require('turndown')
-const turndownService = new TurndownService()
-const turndownPluginGfm = require('turndown-plugin-gfm')
-const gfm = turndownPluginGfm.gfm
-const tables = turndownPluginGfm.tables
-const strikethrough = turndownPluginGfm.strikethrough
-turndownService.use([gfm, tables, strikethrough])
-turndownService.addRule('addCode', {
-  filter: ['h1'],
-  replacement: function(content) {
-    return '# ' + regWord(content)
-  }
-})
+const path = require('path');
+const { turndownService } = require('./lib/turndownService')
+const inputPath = path.resolve(process.cwd(), process.argv[2])
+const outputPath = process.argv[3]
 
-const MarkdownIt = require('markdown-it');
-const md = new MarkdownIt();
+const mark = fs.readFileSync(inputPath, 'utf-8')
 
-const mark = fs.readFileSync('./test.md', 'utf-8');
-const token = md.parse(mark)
-const html = md.renderer.render(token);
-
+const marked = require('marked')
+marked.setOptions({ breaks: true })
+const html = marked(mark)
 const markdown = turndownService.turndown(html)
-fs.writeFile('test1.md', markdown, 'utf8', (err) => {
+
+fs.writeFile(outputPath, markdown, 'utf8', (err) => {
   if (err) throw err;
-  console.log('文件已被保存');
+  console.log('文件已生成');
 });
-
-
-function insertStr(source, start, newStr) {
-  return source.slice(0, start) + source.slice(start).replace(newStr, "`" + newStr + "`");
-}
-
-function regWord(text) {
-  let reg = /(\.|\w|\+|\-|\*|\s|\%|\/)+/g;
-  let regResult = null
-  let results = []
-  let result = text
-  while ((regResult = reg.exec(text)) != null) {
-    if (regResult[0].trim()) {
-      results.push({ key: regResult[0].trim(), index: regResult.index })
-    }
-  }
-
-  if (results.length) {
-    results.map(({ key, index }, i) => {
-      if (i > 0) {
-        index += 2
-      }
-      result = insertStr(result, index, key)
-    })
-
-  }
-
-  return result
-}
